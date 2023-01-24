d939de00d8baf6af69626f3e1b21beb535458833
yuchieh8968 <yuchieh@ualberta.ca>
2023-01-23 22:15:04 -0700

    recognize 405 and 200

--- server.py
+++ server.py
@@ -26,13 +26,39 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+HTTPCODE = {"200":"OK",
+            "301": "Paths Moved",
+            "404":"Paths Not Found",
+            "405": "Method Not Allowed"}
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
+        self.addr = self.parse(self.data)
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        self.respondcode = ""
+        if "GET" in self.addr[0]:
+            self.respondcode = "200"
+        else:
+            self.respondcode = "405"
+
+        self.request.send(bytearray("HTTP/1.1 200 OK\n\n", 'utf-8'))
+        self.request.send(bytearray("HTTP/1.1 " + self.respondcode + " " + HTTPCODE[self.respondcode] + "\n\n", 'utf-8'))
+
+    def parse(self, data):
+        self.parsed = []
+        print(self.data)
+        # b'GET /index.html HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: curl/7.79.1\r\nAccept: */*'
+        # ["b'GET", '/index.html', 'HTTP/1.1\\r\\nHost:', 'localhost:8080\\r\\nUser-Agent:', 'curl/7.79.1\\r\\nAccept:', "*/*'"]
+        self.parsed = str(data).split(" ")
+
+        return self.parsed
+
+    def send_request(self, host, port, request_data):
+        pass
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +70,17 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+
+
+# wsl ubuntu
+# only show html and css
+# reject png
+#
+# 1. curl get familiar with
+# 2. how to parsh; \n\n cut out
+# 3. index.html assumed the end pt of addr if not specified
+#
+# after parse; give proper http responce
+
+
